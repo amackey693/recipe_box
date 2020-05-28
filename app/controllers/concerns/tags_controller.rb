7c772a8c171a525680c6aa96450ca1aa00201be6
Allison Mackey <a.mackey693@gmail.com>
2020-05-28 14:36:28 -0700

    add routing and views for recipes & tags

---
+++ app/controllers/concerns/tags_controller.rb
@@ -0,0 +1,42 @@
+class TagsController
+
+  def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @tag = @recipes.tags.new
+    render :new
+  end
+
+  def create
+     @recipe = Recipe.find(params[:recipe_id])
+     @tag = @recipes.tags.new(tag_params)
+     if @tag.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:album_id])
+    @tag = Tag.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @recipe = Recipe.find(params[:album_id])
+    @tag = Tag.find(params[:id])
+    render :show
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    if @tag.update(tag_params)
+      redirect_to recipe_path
+  end
+
+  def destroy
+   @tag = Tag.find(params[:id])
+  @tag.destroy
+  redirect_to reci_path(@song.album)
+  end
+end
